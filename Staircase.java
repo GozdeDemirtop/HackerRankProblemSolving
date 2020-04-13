0625c62cf7a0d0679c89a67325be2f4f4fb9ac6f
Gozde <gozdedemirtop@gmail.com>
2020-04-13 14:36:15 +0300

    Staircase

---
+++ Staircase.java
@@ -0,0 +1,78 @@
+/*Consider a staircase of size n =4:
+
+   #
+  ##
+ ###
+####
+Observe that its base and height are both equal to n, and the image is drawn using # symbols and spaces.
+The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n.
+
+Function Description
+
+Complete the staircase function in the editor below. It should print a staircase as described above.
+
+staircase has the following parameter(s):
+
+n: an integer
+Input Format
+
+A single integer, n, denoting the size of the staircase.
+
+Output Format
+
+Print a staircase of size n using # symbols and spaces.
+
+Note: The last line must have 0 spaces in it.
+
+Sample Input
+
+6
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Staircase {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+
+        for(int i=1; i<=n; i++){
+            for(int k=n-i; k>=1; k--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
