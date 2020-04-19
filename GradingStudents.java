fb0e3796940b9e37b49cf24d3a05f902d418e46d
Gozde <gozdedemirtop@gmail.com>
2020-04-19 17:39:30 +0300

    Grading Students

---
+++ GradingStudents.java
@@ -0,0 +1,118 @@
+/*HackerLand University has the following grading policy:
+
+Every student receives a grade in the inclusive range from 0 to 100.
+Any grade less than 40 is a failing grade.
+Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 3.
+If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+For example, grade = 84 will be rounded to 85 but grade = 29 will not be rounded
+because the rounding would result in a number that is less than .
+
+Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.
+
+Function Description
+
+Complete the function gradingStudents in the editor below.
+It should return an integer array consisting of rounded grades.
+
+gradingStudents has the following parameter(s):
+
+grades: an array of integers representing grades before rounding
+Input Format
+
+The first line contains a single integer, n, the number of students.
+Each line i of the n subsequent lines contains a single integer, grades[i], denoting student i's grade.
+
+Output Format
+
+For each grade[i], print the rounded grade on a new line.
+
+Sample Input 0
+
+4
+73
+67
+38
+33
+Sample Output 0
+
+75
+67
+40
+33
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+public class GradingStudents {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+
+        // Write your code here
+
+        for(int i=0; i<grades.size(); i++){
+
+            int originalGrade = grades.get(i);
+            int remain = originalGrade % 5;
+
+            if(originalGrade>=38){
+                if(remain >= 3){
+                    originalGrade += (5 - remain) ;
+                    grades.set(i, originalGrade);
+                }
+            }
+        }
+        return grades;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = GradingStudents.gradingStudents(grades);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
