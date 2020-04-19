18b295e0b11dacd3dd5bfe8f4287ea48930ffa57
Gozde <gozdedemirtop@gmail.com>
2020-04-19 17:42:55 +0300

    Grading Students

--- GradingStudents.java
+++ GradingStudents.java
@@ -87,6 +87,7 @@ public class GradingStudents {
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
